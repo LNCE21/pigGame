5bfb19a87e4da68c3f3075d9fa5aeafbd3380048
LNCE21 <lautaronicolascc@gmail.com>
2021-10-24 14:21:39 -0300

    Added a class to hide the dice, and define variables that selected the score and dice elements

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 'use strict';
 
+//Selecting elements
+const scoreElement0 = document.querySelector("#score--0");
+const scoreElement1 = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+
+//Setting initial conditions of score zero elements
+scoreElement0.textContent = 0;
+scoreElement1.textContent = 0;
+
+//Select the element with class "dice". And add the element with class "hidden" to -Hide the dice-
+diceElement.classList.add("hidden");
 
